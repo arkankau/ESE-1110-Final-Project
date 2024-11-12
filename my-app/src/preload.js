a5cba56a5267a6c35644c0aa1f79a825ad195bbd
Ahmed-Altunkaya <ahmedalt@seas.upenn.edu>
2024-11-12 18:34:45 -0500

    New Code with Camera

--- my-app/src/preload.js
+++ my-app/src/preload.js
@@ -1,2 +1,15 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+
+// preload.js
+window.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('webcam');
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        video.srcObject = stream;
+      })
+      .catch(err => {
+        console.error('Error accessing webcam: ', err);
+      });
+  });
